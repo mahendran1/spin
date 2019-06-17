fb9fa6fd175ed5c470e0146cff3418b9387d9d1b
mahendran1 <49430302+mahendran1@users.noreply.github.com>
2019-06-17 09:36:03 +0530

    rolling back changes

--- main.go
+++ main.go
@@ -6,41 +6,27 @@ import (
 	"os"
 	"fmt"
 	"net/http"
-	"github.com/prometheus/client_golang/prometheus"
-  	"github.com/prometheus/client_golang/prometheus/promhttp"
-  	"log"
-  	"time"
-  	"math/rand
-)
+	)
 
-var (
-  counter = prometheus.NewCounter(
-     prometheus.CounterOpts{
-        Namespace: "golang",
-        Name:      "my_counter",
-        Help:      "This is my counter",
-     })
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Handling %+v\n", r);
+	bs, err := ioutil.ReadFile("/content/index.html")
 
-  gauge = prometheus.NewGauge(
-     prometheus.GaugeOpts{
-        Namespace: "golang",
-        Name:      "my_gauge",
-        Help:      "This is my gauge",
-     })
+	if err != nil {
+		fmt.Printf("Couldn't read index.html: %v", err);
+		os.Exit(1)
+	}
 
-  histogram = prometheus.NewHistogram(
-     prometheus.HistogramOpts{
-        Namespace: "golang",
-        Name:      "my_histogram",
-        Help:      "This is my histogram",
-     })
+	io.WriteString(w, string(bs[:]))
+}
+package main
 
-  summary = prometheus.NewSummary(
-     prometheus.SummaryOpts{
-        Namespace: "golang",
-        Name:      "my_summary",
-        Help:      "This is my summary",
-     })
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"fmt"
+	"net/http"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -60,26 +46,4 @@ func main() {
 	port := ":8000"
 	fmt.Printf("Starting to service on port %s\n", port);
 	http.ListenAndServe(port, nil)
-	rand.Seed(time.Now().Unix())
-
-  http.Handle("/metrics", promhttp.Handler())
-
-  prometheus.MustRegister(counter)
-  prometheus.MustRegister(gauge)
-  prometheus.MustRegister(histogram)
-  prometheus.MustRegister(summary)
-
-  go func() {
-     for {
-        counter.Add(rand.Float64() * 5)
-        gauge.Add(rand.Float64()*15 - 5)
-        histogram.Observe(rand.Float64() * 10)
-        summary.Observe(rand.Float64() * 10)
-
-        time.Sleep(time.Second)
-     }
-  }()
-
-  log.Fatal(http.ListenAndServe(":8080", nil))
-}
 }
